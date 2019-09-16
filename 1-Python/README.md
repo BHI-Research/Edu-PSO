6250784b2f387f1e1f6ccb1498a29b57d641a7b5
Ariel Ivan Diaz <arielivandiaz@gmail.com>
2019-09-16 11:36:39 -0300

    update readme

---
+++ 1-Python/README.md
@@ -0,0 +1,23 @@
+# Edu-PSO Python
+
+
+
+## 1 - Simple Swarm
+
+## 2 - Simple Benchmark
+
+
+### Run benchmark.sh
+```sh
+sh benchmark.sh
+```
+
+###  Results
+
+![Bechmark Graph](https://raw.githubusercontent.com/BHI-Research/Edu-PSO/master/1-Python/Figures/fig1.PNG)
+![Bechmark Graph](https://raw.githubusercontent.com/BHI-Research/Edu-PSO/master/1-Python/Figures/fig2.PNG)
+![Bechmark Graph](https://raw.githubusercontent.com/BHI-Research/Edu-PSO/master/1-Python/Figures/fig3.PNG)
+![Bechmark Graph](https://raw.githubusercontent.com/BHI-Research/Edu-PSO/master/1-Python/Figures/fig4.PNG)
+![Bechmark Graph](https://raw.githubusercontent.com/BHI-Research/Edu-PSO/master/1-Python/Figures/fig5.PNG)
+
+
--- 1-Python/benchmark.sh
+++ 1-Python/benchmark.sh
@@ -1,5 +1,5 @@
-py 2-simple-benchmark.py -bench_flag 1 -bench_step 5 -bench_start 5 -bench_end 100
-py 2-simple-benchmark.py -bench_flag 3 -bench_step 5 -bench_start 5 -bench_end 250
-py 2-simple-benchmark.py -bench_flag 4 -bench_step 0.1 -bench_start 0.1 -bench_end 5
-py 2-simple-benchmark.py -bench_flag 5 -bench_step 0.1 -bench_start 0.1 -bench_end 5
-py 2-simple-benchmark.py -bench_flag 6 -bench_step 0.1 -bench_start 0.1 -bench_end 5
+python 2-simple-benchmark.py -bench_flag 1 -bench_step 5 -bench_start 5 -bench_end 100
+python 2-simple-benchmark.py -bench_flag 3 -bench_step 5 -bench_start 5 -bench_end 250
+python 2-simple-benchmark.py -bench_flag 4 -bench_step 0.1 -bench_start 0.1 -bench_end 5
+python 2-simple-benchmark.py -bench_flag 5 -bench_step 0.1 -bench_start 0.1 -bench_end 5
+python 2-simple-benchmark.py -bench_flag 6 -bench_step 0.1 -bench_start 0.1 -bench_end 5
